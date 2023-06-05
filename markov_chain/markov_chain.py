8620d1349c7aadee3c4b58167cc04b669406313d
Chris Howes <chrishowes36@gmail.com>
2023-06-05 15:03:05 +0100

    fixed original issues but still not great

--- markov_chain/markov_chain.py
+++ markov_chain/markov_chain.py
@@ -11,14 +11,14 @@ def calculate_points(I_0, N, T, timestep, beta, gamma):
         Sprime = S[-1]
         Iprime = I[-1]
         r = random.uniform(0, 1)
-        if r < (beta * (S[-1] * I[-1]) / N) * timestep:
+        p = (beta * (S[-1] * I[-1]) / N) * timestep
+        p %= 1
+        q = gamma * I[-1] * timestep
+        q %= 1
+        if r < p:
             Sprime -= 1
             Iprime += 1
-        elif (
-            r
-            < (beta * (S[-1] * I[-1]) / N) * timestep
-            + gamma * I[-1] * timestep
-        ):
+        elif r < p + q:
             Iprime -= 1
         Rprime = N - Sprime - Iprime
         S.append(Sprime)
@@ -31,7 +31,7 @@ N = 2000
 I_0 = 15
 beta = 0.6
 gamma = 0.3
-T = 100 * 24  # 100 days in seconds
+T = 100 * 24 * 60  # 100 days in seconds
 timestep = 1
 S, I, R = calculate_points(I_0, N, T, timestep, beta, gamma)
 Trange = np.arange(0, T, timestep)
